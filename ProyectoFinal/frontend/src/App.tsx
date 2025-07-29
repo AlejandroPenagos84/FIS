4c157987f52307d900731cc1015cea3fd5ce29de
AlejandroPenagos84 <alejopenagos79@gmail.com>
2025-07-28 20:06:19 -0500

    Agrego register

--- ProyectoFinal/frontend/src/App.tsx
+++ ProyectoFinal/frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login/>
+      <Register/>
     </>
   )
 }
